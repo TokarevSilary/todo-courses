951b93a6a371a73413c5a2f138f9ab1fb90da49e
Sergei Soletskii <serey19992@yandex.ru>
2026-04-26 01:00:08 +0300

    Second commit

--- task.js
+++ task.js
@@ -1,69 +1,84 @@
 const form = document.querySelector('form');
 const tasksDaily = document.querySelector(".tasks__input");
-let myStorage = window.localStorage;
+const myStorage = window.localStorage;
+const tasks = document.getElementById("tasks");
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (tasksDaily.value !== "") {
+    if (tasksDaily.value.trim() !== "") {
         updateInformation();
     }
 })
 
 
 function updateInformation() {
-
-    let ourTextBlock = document.createElement("div");
-    ourTextBlock.classList.add("tasks__list");
-    ourTextBlock.id = "tasks__list";
-
-    let textBlock = document.createElement("div");
-    textBlock.classList.add("task");
-
-    let block = document.createElement("div");
-    block.classList.add("task__title");
-
-    ourTextBlock.append(textBlock);
-    textBlock.append(block);
-    block.innerHTML = tasksDaily.value;
-
-    const cross = document.createElement("a");
-    cross.classList.add("task__remove");
-    cross.href = "#";
-    cross.innerHTML = "&times";
-    console.log("я туту");
-    let id = Date.now();
-    cross.dataset.id = String(id);
-    cross.addEventListener("click", (e) => {
-        e.preventDefault();
-        cross.closest('.tasks__list').remove();
-    })
-    block.insertAdjacentElement("afterend", cross);
-
-
-    form.insertAdjacentElement("afterend", ourTextBlock);
-
+    createTask()
     tasksDaily.value = "";
-
-    // const ourSaveFile = ourTextBlock.outerHTML;
-    // localStorage.setItem(`${id}`, ourSaveFile);
-    // console.log(myStorage);
+    saveFile()
 }
 
-// if (myStorage.length > 0) {
-//     for (let i = 0; i < myStorage.length; i++) {
-//         const key = localStorage.key(i);
-//         form.insertAdjacentHTML("afterend", myStorage.getItem(key));
-//     }
-// }
+function createTask() {
+    const tasksList = document.createElement("div");
+    tasksList.classList.add("tasks-list");
+    tasks.append(tasksList);
 
-// document.addEventListener("click", (e) => {
-//     if (e.target.classList.contains("task__remove")) {
-//         e.preventDefault();
-//         let task = e.target.closest(".tasks__list");
-//         task.remove();
-//         let id = e.target.dataset.id;
-//         myStorage.removeItem(id);
-//     }
-// })
+
+    tasksList.insertAdjacentHTML("afterend",
+        `
+            <div class="task">
+              <div class="task__title">
+                ${tasksDaily.value}
+              </div>
+              <a href="#" class="task__remove">&times;</a>
+            </div>
+            `
+    );
+}
+
+function saveFile() {
+    ourSaveFile = [];
+
+    saveFile = document.querySelectorAll(".task__title");
+    saveFile.forEach(element => {
+        ourSaveFile.push(element.textContent.trim());
+    })
+
+    localStorage.setItem("saves", JSON.stringify(ourSaveFile));
+}
+
+
+
+
+if (myStorage.getItem("saves")) {
+    const saves = JSON.parse(myStorage.getItem("saves")) || [];
+    for (let i = 0; i < saves.length; i++) {
+        form.insertAdjacentHTML("afterend",
+            `
+            <div class="task">
+              <div class="task__title">
+                ${saves[i]}
+              </div>
+              <a href="#" class="task__remove">&times;</a>
+            </div>
+            `
+        );
+    }
+}
+
+
+tasks.addEventListener("click", (e) => {
+        if (e.target.classList.contains("task__remove")) {
+            e.preventDefault();
+            let task = e.target.closest(".task");
+            let ourText = task.querySelector(".task__title");
+            task.remove();
+            const saves = JSON.parse(myStorage.getItem("saves")) || [];
+            let newSaves =saves.filter(element => element.trim() !== ourText.textContent.trim());
+            myStorage.setItem("saves", JSON.stringify(newSaves));
+
+        }
+    })
+
+
 
 
 
